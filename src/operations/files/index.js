14b9647af3f14c65643eb4f0a466a03ecdcc55cf
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-04 01:38:17 +0300

    feat: implement mv operation

--- src/operations/files/index.js
+++ src/operations/files/index.js
@@ -2,8 +2,9 @@ import add from "./add.js";
 import cat from "./cat.js";
 import cp from "./cp.js";
 import mkdir from "./mkdir.js";
+import mv from "./mv.js";
 import rm from "./rm.js";
 import rn from "./rn.js";
-const fileOperations = [cat, add, mkdir, rm, rn, cp];
+const fileOperations = [cat, add, mkdir, rm, rn, cp, mv];
 
 export default fileOperations;
